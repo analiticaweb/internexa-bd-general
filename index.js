9db7560fe8a878420563c384bb366c481c7eb0c4
David Botero Salazar <analiticaweb@pragma.com.co>
2020-04-06 10:11:22 -0500

    adding question

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const hbs = require('hbs');
 
 
 const app = express();
-app.set('view engine','hbs');
+app.set('view engine', 'hbs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -17,15 +17,15 @@ const port = process.env.PORT || 3000;
 var auth = { apiKey: 'rRqs0ZRCR5SpvpBcEihc/MH9y3zI+htzXcOLnkWraOHM6c9qR6RL/jDpYepA73jmHvxxPD4H2E7ypjmjW4VGxWnRIq2V1ekMqWY5P52VHYvHZGcoNQV6SFsUdradTbGrFj9aJPSKt2pnK4S/WbEGiQ==' };
 var api = new createsend(auth);
 var listId = 'dabb329b9c92c5f5262377d0c5441659' // The ID of the list
-var testList= '7d3ca8bca6a63c69c783ab4e2217695a'
+var testList = '7d3ca8bca6a63c69c783ab4e2217695a'
 
 
 app.post("/send-user", (req, res) => {
     var user = {
         EmailAddress: req.body.email,
         CustomFields: [
-            { Key: 'asesoria', Value: 1 }
-          ]
+            { Key: 'asesoria', Value: req.body.asesoria }
+        ]
     };
     api.subscribers.addSubscriber(testList, user, (err, response) => {
         if (err) {
@@ -33,22 +33,35 @@ app.post("/send-user", (req, res) => {
             res.send(err)
         }
         else {
-            console.log(response)
-            console.log(user);
-            res.render('sendResponse',{email:user.EmailAddress});
+            if (req.body.asesoria == 1) {
+                res.render('sendResponse', { email: user.EmailAddress });
+            } else {
+                res.render('thank-you');
+            }
+
         }
     });
 
 })
 
-app.post("/thank-you",(req,res)=>{
+app.get('/send-user', (req, res) => {
+    res.render('sendResponse');
+})
+app.get('/thank-you', (req, res) => {
+    res.render('thank-you');
+})
+app.get('/question', (req, res) => {
+    res.render('question');
+})
+
+app.post("/thank-you", (req, res) => {
     var user = {
         EmailAddress: req.body.email,
         CustomFields: [
             { Key: 'company', Value: req.body.company },
             { Key: 'phone', Value: req.body.phone },
             { Key: 'descripcionAsesoria', Value: req.body.descripcionAsesoria }
-          ]
+        ]
     };
     api.subscribers.addSubscriber(testList, user, (err, response) => {
         if (err) {
